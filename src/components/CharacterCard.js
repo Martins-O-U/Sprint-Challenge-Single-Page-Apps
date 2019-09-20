d722581d19e46d367e1bc5a48b01929a3be124c7
Martins-O-U <martinsonyedikachi@gmail.com>
2019-09-20 08:41:07 +0100

    reactstrap installed and imported

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,4 +1,8 @@
 import React from "react";
+import {
+  Card, CardImg, CardText, CardBody,
+  CardTitle, CardSubtitle, Button
+} from 'reactstrap';
 
 export default function CharacterCard() {
   return <span>todo: character</span>;
