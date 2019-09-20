a6801f03bdbe358a5ddc20c41738fbd9cb0129a8
Martins-O-U <martinsonyedikachi@gmail.com>
2019-09-20 09:30:24 +0100

    Data fetched and displayed on the browser as cards

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,9 +1,23 @@
 import React from "react";
 import {
   Card, CardImg, CardText, CardBody,
-  CardTitle, CardSubtitle, Button
+  CardTitle, CardSubtitle,
 } from 'reactstrap';
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+export default function CharacterCard({data}) {
+  console.log(data.name)
+  return (
+    <div className='card-body'>
+      <Card>
+        <CardImg top width="100%" src={data.image} alt="Card image cap" />
+        <CardBody>
+          <div className="card-content">
+            <CardTitle><strong>{data.name}</strong></CardTitle>
+            <CardSubtitle>({data.gender})</CardSubtitle>
+          </div>
+          {/* <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText> */}
+        </CardBody>
+      </Card>
+    </div>
+  );
 }
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,17 +1,40 @@
 import React, { useEffect, useState } from "react";
 import CharacterCard from "./CharacterCard";
+import axios from "axios";
 
-export default function CharacterList() {
+export default function CharacterList(props) {
   // TODO: Add useState to track data from useEffect
+  const [character, setCharacter] = useState([]);
+  console.log(character);
 
+  // const id = props.match.params.id;
+  const characterUrl =  `https://rickandmortyapi.com/api/character/`;
   useEffect(() => {
+
+
     // TODO: Add API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+
+    axios.get(characterUrl)
+    .then(response => {
+        // debugger
+        let characterFromAPI = response.data.results;
+        console.log(characterFromAPI);
+        setCharacter(characterFromAPI);
+         
+    })
+    .catch(err => {
+        // debugger
+        console.log(err.message);
+    });
   }, []);
 
   return (
-    <section className="character-list">
-      <h2>TODO: `array.map()` over your state here!</h2>
+    <section className="character-list grid-view">
+      {character
+      ? character.map(charac => <CharacterCard data={charac} key={charac.id} />)
+      : null}
+      {/* <h2>TODO: `array.map()` over your state here!</h2> */}
     </section>
   );
 }
